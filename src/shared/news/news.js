19cde97171afe251bb61712cccceebdd132c5c76
John Nguyen <aznxmist@gmail.com>
2023-07-23 13:40:45 -0500

    Changed structure of changelog

--- src/shared/news/news.js
+++ src/shared/news/news.js
@@ -1,161 +1,37 @@
 const NewsArticles = [
     {
         id: 0,
-        new: "new",
-        title: "Changelogs - Updated July 21, 2023",
-        category: "Site News",
-        date: "July 21, 2023",
-        preview: "/chara_face_1200_1.png",
-        article:`
-            <h3>Changelog - July 21, 2023</h3>
-            <p>Hey Summoners, we have yet another Collab on Japan. This time it is for Hell's Paradise</p>
-            <br/>
-            <p><b>Here are today's additions:</b></p>
-            <br/><br/>
-            <p>Following Units have been added:</p>
-            <a href="https://www.grandsummoners.info/units/Gabimaru"><img src="/db/Units/Thumbnail/unit_thumbnail_107797112.png" /></a>
-            <a href="https://www.grandsummoners.info/units/Sagiri"><img src="/db/Units/Thumbnail/unit_thumbnail_107807312.png" /></a>
-            <br/><br/>
-            <p>Following Equips have been added:</p>
-            <a href="https://www.grandsummoners.info/equips/画眉丸の服"><img src="/db/Equipment/Thumbnail/item_thumbnail_152651.png" /></a>
-            <a href="https://www.grandsummoners.info/equips/佐切の刀"><img src="/db/Equipment/Thumbnail/item_thumbnail_152671.png" /></a>
-            <a href="https://www.grandsummoners.info/equips/佐切の服"><img src="/db/Equipment/Thumbnail/item_thumbnail_152681.png" /></a>
-            <a href="https://www.grandsummoners.info/equips/画眉丸の足袋"><img src="/db/Equipment/Thumbnail/item_thumbnail_152661.png" /></a>
-            <a href="https://www.grandsummoners.info/equips/佐切の髪飾り"><img src="/db/Equipment/Thumbnail/item_thumbnail_152701.png" /></a>
-            <a href="https://www.grandsummoners.info/equips/佐切の篭手"><img src="/db/Equipment/Thumbnail/item_thumbnail_152691.png" /></a>
-            <a href="https://www.grandsummoners.info/equips/画眉丸のマスク"><img src="/db/Equipment/Thumbnail/item_thumbnail_152631.png" /></a>
-            <a href="https://www.grandsummoners.info/equips/極『画眉丸のマスク』"><img src="/db/Equipment/Thumbnail/item_thumbnail_152641.png" /></a>
-            <br/>
-            <p>That's all for today. Until next time!</p>
-            <p>Thanks, <br/> GSOD Staff</p>
-            <h3>Changelog - July 14, 2023</h3>
-            <p>Hey Summoners, Fate Collab has released on Global. And with that, we are updating the collaboration info to Global names.</p>
-            <p>I'm sure some of you have noticed we haven't put the SP illustrations yet. No worries they are coming.</p>
-            <p>In addition, the changelog will eventually be a bit easier to read, however I'm having difficulty finding free time for this. So it might take longer than expected</p>
-            <br/>
-            <p><b>Here are today's additions:</b></p>
-            <br/><br/>
-            <p>Following Units have been updated to Global Filters:</p>
-            <a href="https://www.grandsummoners.info/units/Illya"><img src="/db/Units/Thumbnail/unit_thumbnail_107737412.png" /></a>
-            <a href="https://www.grandsummoners.info/units/Rin%20&%20Luvia"><img src="/db/Units/Thumbnail/unit_thumbnail_107767112.png" /></a>
-            <br/><br/>
-            <p>Following Equips have been updated to Global Filters:</p>
-            <a href="https://www.grandsummoners.info/equips/Excalibur"><img src="/db/Equipment/Thumbnail/item_thumbnail_152341_pris.png" /></a>
-            <a href="https://www.grandsummoners.info/equips/True%20'Magical%20Ruby'"><img src="/db/Equipment/Thumbnail/item_thumbnail_152311_pris.png" /></a>
-            <a href="https://www.grandsummoners.info/equips/Magical%20Ruby'"><img src="/db/Equipment/Thumbnail/item_thumbnail_152331_pris.png" /></a>
-            <a href="https://www.grandsummoners.info/equips/Rin%20Tohsaka's%20Clothes"><img src="/db/Equipment/Thumbnail/item_thumbnail_152371.png" /></a>
-            <a href="https://www.grandsummoners.info/equips/Illya's%20Clothes"><img src="/db/Equipment/Thumbnail/item_thumbnail_152381.png" /></a>
-            <a href="https://www.grandsummoners.info/equips/Jeweled%20Sword%20Zelretch'"><img src="/db/Equipment/Thumbnail/item_thumbnail_152361.png" /></a>
-            <a href="https://www.grandsummoners.info/equips/True%20'Jeweled%20Sword%20Zelretch'"><img src="/db/Equipment/Thumbnail/item_thumbnail_152321_pris.png" /></a>
-            <a href="https://www.grandsummoners.info/equips/Illya's%20Saber%20Dress"><img src="/db/Equipment/Thumbnail/item_thumbnail_152351.png" /></a>
-            <br/>
-            <p>That's all for today. Until next time!</p>
-            <p>Thanks, <br/> GSOD Staff</p>
-
-            
-            <h3>Changelog - July 12, 2023</h3>
-            <p><b>Here are today's additions:</b></p>
-            <br/><br/>
-            <p>Following Unit has been added:</p>
-            <a href="https://www.grandsummoners.info/units/Summer%20Iris"><img src="/db/Units/Thumbnail/unit_thumbnail_103727112.png" /></a>
-            <br/><br/>
-            <p><b>Following Equip has been added:</b></p>
-            <a href="https://www.grandsummoners.info/equips/イリスのドリームロケット"><img src="/db/Equipment/Thumbnail/item_thumbnail_111601.png" /></a>
-            <br/>
-            <p>That's all for today. Until next time!</p>
-            <p>Thanks, <br/> GSOD Staff</p>
-            <br/>
-            <br/>
-            <h3>Changelog - July 4, 2023</h3>
-            <p><b>Here are today's additions:</b></p>
-            <br/><br/>
-            <p>Following Unit has been added to Global filter</p>
-            
-            <a href="https://www.grandsummoners.info/units/Ginzo:%20Battle%20Form"><img src="/db/Units/Thumbnail/unit_thumbnail_103506511.png" /></a>
-            <br/><br/>
-            <p>Following Units have been updated to match JP's stats</p>
-            
-            <a href="https://www.grandsummoners.info/units/Mizuki"><img src="/db/Units/Thumbnail/unit_thumbnail_102826212.png" /></a>
-            <a href="https://www.grandsummoners.info/units/Ginzo"><img src="/db/Units/Thumbnail/unit_thumbnail_102836512.png" /></a>
-            <a href="https://www.grandsummoners.info/units/Shuri"><img src="/db/Units/Thumbnail/unit_thumbnail_102856512.png" /></a>
-            <a href="https://www.grandsummoners.info/units/Amane"><img src="/db/Units/Thumbnail/unit_thumbnail_102866412.png" /></a>
-            <a href="https://www.grandsummoners.info/units/Kisaragi"><img src="/db/Units/Thumbnail/unit_thumbnail_102876112.png" /></a>
-            <a href="https://www.grandsummoners.info/units/Saku"><img src="/db/Units/Thumbnail/unit_thumbnail_102886312.png" /></a>
-            <br/><br/>
-            <p>Following Equips have been updated to match JP's stats</p>
-            <br/>
-            <a href="https://www.grandsummoners.info/equips/Forbidden%20'Demonic%20Seal'"><img src="/db/Equipment/Thumbnail/item_thumbnail_106701.png" /></a>
-            <a href="https://www.grandsummoners.info/equips/True%20'Demonic%20Seal'"><img src="/db/Equipment/Thumbnail/item_thumbnail_106721.png" /></a>
-            <a href="https://www.grandsummoners.info/equips/Ninja%20Spear%20'Netherworld%20Silver'"><img src="/db/Equipment/Thumbnail/item_thumbnail_106711.png" /></a>
-            <a href="https://www.grandsummoners.info/equips/True%20'Netherworld%20Silver'"><img src="/db/Equipment/Thumbnail/item_thumbnail_106731.png" /></a>
-            <a href="https://www.grandsummoners.info/equips/Storm%20Blade%20'TSUJI-KAZE'"><img src="/db/Equipment/Thumbnail/item_thumbnail_106751.png" /></a>
-            <a href="https://www.grandsummoners.info/equips/True%20'TSUJI-KAZE'"><img src="/db/Equipment/Thumbnail/item_thumbnail_106771.png" /></a>
-            <a href="https://www.grandsummoners.info/equips/True%20'Will-o'-the-Wisp'"><img src="/db/Equipment/Thumbnail/item_thumbnail_106761.png" /></a>
-            <a href="https://www.grandsummoners.info/equips/Oni%20Arm%20'Crimson%20Talon'"><img src="/db/Equipment/Thumbnail/item_thumbnail_106981.png" /></a>
-            <a href="https://www.grandsummoners.info/equips/True%20'Crimson%20Talon'"><img src="/db/Equipment/Thumbnail/item_thumbnail_106991.png" /></a>
-            <a href="https://www.grandsummoners.info/equips/Mech%20Blades%20'Dual%20Thunderbolts'"><img src="/db/Equipment/Thumbnail/item_thumbnail_107001.png" /></a>
-            <a href="https://www.grandsummoners.info/equips/True%20'Dual%20Thunderbolts'"><img src="/db/Equipment/Thumbnail/item_thumbnail_107011.png" /></a>
-            <a href="https://www.grandsummoners.info/equips/Ninja%20Garb%20'Mirage%20Kimono'"><img src="/db/Equipment/Thumbnail/item_thumbnail_107021.png" /></a>
-            <a href="https://www.grandsummoners.info/equips/Demon%20Edge%20'Blood%20Moon'"><img src="/db/Equipment/Thumbnail/item_thumbnail_107041.png" /></a>
-            <br/>
-            <p>That's all for today. Until next time!</p>
-            <p>Thanks, <br/> GSOD Staff</p>
-            
-            <br/><br/>
-            <h3>Changelog - July 1, 2023</h3>
-            <p>Hey Summoners, Site Admin here. Summer events are starting, and with that, is a new unit.</p>
-            <p><b>Here are today's additions:</b></p>
-            <br/>
-            <p>Added the following Unit: </p>
-            <br/>
-            <a href="https://www.grandsummoners.info/units/Summer%20Fen"><img src="/db/Units/Thumbnail/unit_thumbnail_103737212.png" /></a>
-            <br/>
-            <p>Added the following Equips: </p>
-            <br/>
-            <a href="https://www.grandsummoners.info/equips/クレイヴ・ライトニング"><img src="/db/Equipment/Thumbnail/item_thumbnail_111531.png" /></a>
-            <a href="https://www.grandsummoners.info/equips/ミラの巫女ミニビキニ"><img src="/db/Equipment/Thumbnail/item_thumbnail_111541.png" /></a>
-            <a href="https://www.grandsummoners.info/equips/ケインのインディペンデンスサマー"><img src="/db/Equipment/Thumbnail/item_thumbnail_111551.png" /></a>
-            <a href="https://www.grandsummoners.info/equips/セラのサマーアテンダント"><img src="/db/Equipment/Thumbnail/item_thumbnail_111561.png" /></a>
-            <a href="https://www.grandsummoners.info/equips/マリカの毬柄マリンワンピ"><img src="/db/Equipment/Thumbnail/item_thumbnail_111571.png" /></a>
-            <a href="https://www.grandsummoners.info/equips/シャルのズッ友フリル"><img src="/db/Equipment/Thumbnail/item_thumbnail_111581.png" /></a>
-            <a href="https://www.grandsummoners.info/equips/ヴァレリーのズッ友フリル"><img src="/db/Equipment/Thumbnail/item_thumbnail_111591.png" /></a>
-            <a href="https://www.grandsummoners.info/equips/フェンのビッグウェーブ"><img src="/db/Equipment/Thumbnail/item_thumbnail_111611.png" /></a>
-            <a href="https://www.grandsummoners.info/equips/天間器『ゼクスラスタ』レプリカ"><img src="/db/Equipment/Thumbnail/item_thumbnail_111621.png" /></a>
-            <a href="https://www.grandsummoners.info/equips/天間器『ゼクスラスタ』アレンジ"><img src="/db/Equipment/Thumbnail/item_thumbnail_111631.png" /></a>
-            <br/>
-            <p>That's all for today. Until next time!</p>
-            <p>Thanks, <br/> GSOD Staff</p>
-            <br/>
-            <h3>Changelog - June 24, 2023</h3>
-            <p>Hey Summoners, Site Admin here. We been in "grand opening" mode for over a year now, so probably best to actually update this.</p>
-            <p>We'll be adding site updates here so you can keep up to date on what's been going on with the website.</p>
-            <br/>
-            <p>Over the past year, we added the Tierlist, Equip List, and Ascended units, while also allowing you to Sort and Filter the Unit and Equip lists.</p>
-            <p>Please note that we will do our best to keep the website up to date as quickly as possible, but we are merely human and we also have Jobs and real life to attend to. As such, there may be a couple days where the updates are slightly delayed. Please bear with us.</p>
-            <br/>
-            <p><b>Anyway, without further ado, here are today's additions: </b><p>
-            <br/>
-            
-            <p>Added Dream Evolutions and JP Buffs to the Following Units:</p>
-            <br/>
-            <a href="https://www.grandsummoners.info/units/Arosdea"><img src="/db/Units/Thumbnail/unit_thumbnail_101176112.png" /></a>
-            <a href="https://www.grandsummoners.info/units/Dargeon"><img src="/db/Units/Thumbnail/unit_thumbnail_100566112.png" /></a>
-            <a href="https://www.grandsummoners.info/units/Angelas"><img src="/db/Units/Thumbnail/unit_thumbnail_103066112.png" /></a>
-            <a href="https://www.grandsummoners.info/units/Herck"><img src="/db/Units/Thumbnail/unit_thumbnail_100576212.png" /></a>
-            <a href="https://www.grandsummoners.info/units/Valhalla"><img src="/db/Units/Thumbnail/unit_thumbnail_100586312.png" /></a>
-            <a href="https://www.grandsummoners.info/units/Eagle"><img src="/db/Units/Thumbnail/unit_thumbnail_103076312.png" /></a>
-            <a href="https://www.grandsummoners.info/units/Melia%20(Dark)"><img src="/db/Units/Thumbnail/unit_thumbnail_101076511.png" /></a>
-            <br/>
-            <b>NOTE: The unit skillsets show GL stats by default, please click "Japan" Tab to see the Dream Evolutions and the JP skillsets. This option will appear ONLY if the unit has any differences between the servers</b> <br/><br/>
-        
-            <p>Additionally, I want to ask for your help in bug-catching.</p>
-            <p>If you notice any issues like a unit page not loading, or Lore is missing (please provide screenshot of the lore), etc, please contact the #discord-feedback channel on the GSOD Discord. </p>
-            <p>This will ensure that the bug fixes are deployed quicker.</p>
-            <p>Fortunately, some users are already doing this and I greatly appreciate it.</p>
-            <br/>
-            <p>That's all for today. Until next time!</p>
-            <p>Thanks, <br/> GSOD Staff</p>
-        `
+        date: `July 21, 2023`,
+        article: 
+        {
+            intro: `Hey Summoners, we have yet another Collab on Japan. This time it is for Hell's Paradise.
+                    Additionally, we revamped the Changelogs to be a bit cleaner. We also removed the Twitter because of the API changes.
+                    The previous changelogs from before today's date will be removed, but in the future, any older changelogs will appear below the newest log.`,
+            new: 
+                {
+                    units: 
+                    [
+                        [`Gabimaru`, 107797112], 
+                        [`Sagiri`, 107807312]
+                    ],
+                    equips: 
+                    [
+                        [`画眉丸の服`, 152651],
+                        [`佐切の刀`, 152671],
+                        [`佐切の服`, 152681],
+                        [`画眉丸の足袋`, 152661],
+                        [`佐切の髪飾り`, 152701],
+                        [`佐切の篭手`, 152691],
+                        [`画眉丸のマスク`, 152631],
+                        [`極『画眉丸のマスク`, 152641]
+                    ]
+                },
+            globalUpdate:
+            {
+                units: [],
+                equips: []
+            }
+        }
     }
 ]
 
@@ -166,7 +42,7 @@ export function getNews() {
 //Using == instead of === to match string to num
 export function getNewsById(id) {
     return NewsArticles.find(
-        newsArticle => newsArticle.id == id
+        newsArticle => newsArticle.date === id
     );
 }
 
