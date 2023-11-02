3633c5d7cc3629ff7161977f8a41dc57dcd948b6
Jeffers321 <jefersng@gmail.com>
2023-11-02 14:56:20 +0800

    Added Global GSlayer break buff

--- src/shared/unitInfo.js
+++ src/shared/unitInfo.js
@@ -26225,7 +26225,7 @@ const UnitInformation = [
                 arts: "22000% Water DMG (PHY). For 20s, reduce Enemies' Break RES and DMG RES by 30%.",
                 artsbreak: 2800,
                 truearts: "75000% Dark DMG (PHY). The less HP, the more DMG & Break PWR during True Arts (up to 300%) + For 60s, increases own Arts by 3/s.",
-                trueartsbreak: 3000
+                trueartsbreak: 5000
             },
         skillsetJP: 
             {
