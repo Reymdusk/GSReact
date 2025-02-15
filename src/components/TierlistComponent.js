49d6a1444c28dd7fae838361e7a2bf6ed30470e9
John Nguyen <aznxmist@gmail.com>
2025-02-15 11:29:09 -0600

    Fixed thumbnails if Super exists for reliance situations

--- src/components/TierlistComponent.js
+++ src/components/TierlistComponent.js
@@ -421,7 +421,7 @@ function DisplayUnit({unit, imageSize, fontSizing}) {
                 <center>
                     <Card style={{border: "none", width: "90px", backgroundColor: "rgba(0, 0, 0, 0)", padding: "0px", margin: "0px"}}>
                         <a href={`/units/${unit.name}/`} style={{textDecoration: "none"}}>
-                            <img src={unit.image.thumbawk} style={thumbnailSize} object-fit="cover" alt={unit.name} />
+                            <img src={unit.image.thumbsuper ? unit.image.thumbsuper : unit.image.thumbawk} style={thumbnailSize} object-fit="cover" alt={unit.name} />
                             <br />
                             <p style={{paddingBottom: "2px", marginBottom: "2px", color: "#aaabb8", fontSize: `${fontSizing}`, width: "75px", display: "inline-block"}}>
                                 {unit.name}
@@ -471,7 +471,7 @@ function DisplayUnit({unit, imageSize, fontSizing}) {
             <center>
                 <Card style={{border: "none", width: "90px", backgroundColor: "rgba(0, 0, 0, 0)", padding: "0px", margin: "0px"}}>
                     <a href={`/units/${unit.name}/`} style={{textDecoration: "none"}}>
-                        <img src={unit.image.thumbawk} style={thumbnailSize} object-fit="cover" alt={unit.name} />
+                        <img src={unit.image.thumbsuper ? unit.image.thumbsuper : unit.image.thumbawk} style={thumbnailSize} object-fit="cover" alt={unit.name} />
                         <br />
                         <p style={{paddingBottom: "2px", marginBottom: "2px", color: "#aaabb8", fontSize: `${fontSizing}`, width: "75px", display: "inline-block"}}>
                             {unit.name}
